d4a15188f5fb75353739df794796bd38e609c0c6
lishifeng <lishifenging@outlook.com>
2025-04-17 22:37:15 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver DS18B20
- * @brief     LibDriver DS18B20 is a full function driver of DS18B20 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver DS18B20 is a full-featured driver of DS18B20 and it can run in both MCU and Linux platform.
  * @version   2.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/04/06  <td>2.0      <td>Shifeng Li  <td>format the code
